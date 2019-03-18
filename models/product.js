0d509c4adbbdf4ee480e0ea71f517e61fc1da7d9
Leila <nb.leila10@gmail.com>
2019-03-17 19:09:28 -0500

    Connect to SQLite3 database and render products page

--- models/product.js
+++ models/product.js
@@ -1,23 +1,6 @@
-const fs = require("fs")
-const path = require("path")
-
 const Cart = require("./cart")
 
-const p = path.join(
-  path.dirname(process.mainModule.filename),
-  "data",
-  "products.json"
-)
-
-const getProductsFromFile = cb => {
-  fs.readFile(p, (err, fileContent) => {
-    if (err) {
-      cb([])
-    } else {
-      cb(JSON.parse(fileContent))
-    }
-  })
-}
+const db = require("../util/database")
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -28,52 +11,13 @@ module.exports = class Product {
     this.price = price
   }
 
-  save() {
-    getProductsFromFile(products => {
-      if (this.id) {
-        // If product exist, update it
-        const existingProductIndex = products.findIndex(
-          prod => prod.id === this.id
-        )
-        const updatedProducts = [...products]
-        updatedProducts[existingProductIndex] = this
-        console.log("Updated product: ", this)
-        fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-          console.log(err)
-        })
-      } else {
-        // If new product, add it
-        this.id = Math.random().toString()
-        products.push(this)
-        fs.writeFile(p, JSON.stringify(products), err => {
-          console.log(err)
-        })
-      }
-    })
+  save() {}
+
+  static fetchAll() {
+    return db("products")
   }
 
-  static fetchAll(cb) {
-    getProductsFromFile(cb)
-  }
+  static findById(id, cb) {}
 
-  static findById(id, cb) {
-    getProductsFromFile(products => {
-      const product = products.find(p => p.id === id)
-      cb(product)
-    })
-  }
-
-  static deleteById(id) {
-    getProductsFromFile(products => {
-      const product = products.find(prod => prod.id === id)
-      const updatedProducts = products.filter(prod => prod.id !== id)
-      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
-        if (!err) {
-          Cart.deleteProduct(id, product.price)
-        } else {
-          console.log(err)
-        }
-      })
-    })
-  }
+  static deleteById(id) {}
 }
