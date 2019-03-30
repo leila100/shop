5242c79693c1b3187b09959b135a53ebd6685e9d
Leila <nb.leila10@gmail.com>
2019-03-30 17:32:33 -0500

    Create the user schema and add a new user
    Use that user when creating a new product
    For now, the user is created when server starts (if no user yet) and store it in req

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
 
 const errorController = require("./controllers/error")
-// const User = require("./models/user")
+const User = require("./models/user")
 
 const app = express()
 
@@ -13,17 +13,17 @@ app.set("view engine", "ejs")
 app.set("views", "views")
 
 // Middleware to create a user and its corresponding cart
-// app.use((req, res, next) => {
-//   // Used to save the current user in req
-//   User.findById("5c9d51e809fe2206b492a2d4")
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id)
-//       next()
-//     })
-//     .catch(err => {
-//       console.log(err)
-//     })
-// })
+app.use((req, res, next) => {
+  // Used to save the current user in req
+  User.findById("5c9fe9f430e68927d8fc94e8")
+    .then(user => {
+      req.user = user
+      next()
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
 
 const adminRoutes = require("./routes/admin")
 const shopRoutes = require("./routes/shop")
@@ -40,5 +40,19 @@ mongoose
   .connect(
     "mongodb+srv://Leila:DgZ2MAckNWKZG3M@cluster0-tfqvb.mongodb.net/shop?retryWrites=true"
   )
-  .then(result => app.listen(3000))
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: "Leila",
+          email: "Leila@test.com",
+          cart: {
+            items: []
+          }
+        })
+        user.save()
+      }
+    })
+    app.listen(3000)
+  })
   .catch(err => console.log(err))
