e40fd4048bcda2d76ec54a896fc402486c407d50
Leila <nb.leila10@gmail.com>
2019-04-08 12:27:42 -0500

    Add saving user input and red border in invalid input in login form

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,7 +28,9 @@ exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
-    errorMessage: message
+    errorMessage: message,
+    oldInput: { email: "", password: "" },
+    validationErrors: []
   })
 }
 
@@ -41,15 +43,22 @@ exports.postLogin = (req, res, next) => {
     return res.status(422).render("auth/login", {
       path: "/login",
       pageTitle: "Login",
-      errorMessage: errors.array()[0].msg
+      errorMessage: errors.array()[0].msg,
+      oldInput: { email, password },
+      validationErrors: errors.array()
     })
   }
 
   User.findOne({ email: email })
     .then(user => {
       if (!user) {
-        req.flash("error", "Invalid email or password.")
-        return res.redirect("/login")
+        return res.status(422).render("auth/login", {
+          path: "/login",
+          pageTitle: "Login",
+          errorMessage: "Invalid email or password",
+          oldInput: { email, password },
+          validationErrors: []
+        })
       }
       bcrypt
         .compare(password, user.password)
@@ -63,8 +72,13 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/")
             })
           }
-          req.flash("error", "Invalid email or password.")
-          res.redirect("/login")
+          return res.status(422).render("auth/login", {
+            path: "/login",
+            pageTitle: "Login",
+            errorMessage: "Invalid email or password",
+            oldInput: { email, password },
+            validationErrors: []
+          })
         })
         .catch(err => {
           console.log(err)
