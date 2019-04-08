fa004681f84a58b3468b5338eb11fc1276af1633
Leila <nb.leila10@gmail.com>
2019-04-08 12:15:20 -0500

    Add red border to input if invalid in register form

--- controllers/auth.js
+++ controllers/auth.js
@@ -91,7 +91,8 @@ exports.getSignup = (req, res, next) => {
     path: "/signup",
     pageTitle: "Signup",
     errorMessage: message,
-    oldInput: { email: "", password: "", confirmPassword: "" }
+    oldInput: { email: "", password: "", confirmPassword: "" },
+    validationErrors: []
   })
 }
 
@@ -100,11 +101,13 @@ exports.postSignup = (req, res, next) => {
   const errors = validationResult(req)
 
   if (!errors.isEmpty()) {
+    console.log(errors.array())
     return res.status(422).render("auth/signup", {
       path: "/signup",
       pageTitle: "Signup",
       errorMessage: errors.array()[0].msg,
-      oldInput: { email, password, confirmPassword: req.body.confirmPassword }
+      oldInput: { email, password, confirmPassword: req.body.confirmPassword },
+      validationErrors: errors.array()
     })
   }
 
