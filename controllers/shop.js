52cc490dcc99035243fccbc5697d14088df382ae
Leila <nb.leila10@gmail.com>
2019-04-13 17:28:43 -0500

    Add PDFKit to create a test pdf file in invoices folder

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,6 @@
 const fs = require("fs")
 const path = require("path")
+const PDFDocument = require("pdfkit")
 
 const Product = require("../models/product")
 const Order = require("../models/order")
@@ -150,22 +151,18 @@ exports.getInvoice = (req, res, next) => {
         return next(new Error("Unauthorized"))
       const invoiceName = "invoice-" + orderId + ".pdf"
       const invoicePath = path.join("data", "invoices", invoiceName)
-      // fs.readFile(invoicePath, (err, data) => {
-      //   if (err) return next(err)
-      //   res.setHeader("Content-Type", "application/pdf")
-      //   res.setHeader(
-      //     "Content-Disposition",
-      //     'inline; filename="' + invoiceName + '"'
-      //   )
-      //   res.send(data)
-      // })
-      const file = fs.createReadStream(invoicePath)
+
+      const pdfDoc = new PDFDocument()
       res.setHeader("Content-Type", "application/pdf")
       res.setHeader(
         "Content-Disposition",
         'inline; filename="' + invoiceName + '"'
       )
-      file.pipe(res)
+      pdfDoc.pipe(fs.createWriteStream(invoicePath))
+      pdfDoc.pipe(res)
+
+      pdfDoc.text("Hello World!")
+      pdfDoc.end()
     })
     .catch(err => {
       const error = new Error(err)
